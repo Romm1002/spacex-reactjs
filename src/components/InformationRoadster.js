702ce97c3ed7a818dbf369bc30537999e7e6b058
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2023-05-26 10:37:20 +0200

    fix et crétion de composants pour roadster (#11)

---
+++ src/components/InformationRoadster.js
@@ -0,0 +1,31 @@
+import { Col, ListGroup, Row } from "react-bootstrap"
+import Information from "./Information"
+
+const InformationRoadster = ({ informations }) => {
+
+    console.log(informations)
+    return (
+        <>
+            <ListGroup className='list-group-flush roadster'>
+                <Row className='p-0 m-0 '>
+                    <Information title='Speed' information={informations.speed} />
+                    <Information title='Weight' information={informations.weight} />
+                </Row>
+                <Row className='p-0 m-0 '>
+                    <Information title='Distance from Earth' information={informations.earthDistance} />
+                    <Information title='Distance from Mars' information={informations.marsDistance} />
+                </Row>
+                <Row className='p-0 m-0 '>
+                    <Information title='NORAD identifier' information={informations.norad_id} />
+                    <Information title='Period days' information={informations.period_days} />
+                </Row>
+                <Row className='p-0 m-0 '>
+                    <Information title='Apoapsis' information={informations.apoapsis} />
+                    <Information title='Periapsis' information={informations.periapsis} />
+                </Row>
+            </ListGroup>
+        </>
+    )
+}
+
+export default InformationRoadster
--- src/pages/ListHistory.js
+++ src/pages/ListHistory.js
@@ -1,52 +1,52 @@
-import React, { useState, useEffect } from "react";
-import CardHistory from "../components/CardHistory";
-import { Row } from "react-bootstrap";
-import HttpClient from "../components/HttpClient";
+import React, { useState, useEffect } from 'react'
+import CardHistory from '../components/CardHistory'
+import { Row } from 'react-bootstrap'
+import HttpClient from '../components/HttpClient'
 // Recupérer l'api
 
 const ListHistory = () => {
-  /** -------------- HTTP CLIENT -------------- **/
-  const [error, setError] = useState(null);
-  const [strResponse, setStrResponse] = useState(null);
-  const [response, setResponse] = useState(null);
+    /** -------------- HTTP CLIENT -------------- **/
+    const [error, setError] = useState(null)
+    const [strResponse, setStrResponse] = useState(null)
+    const [response, setResponse] = useState(null)
 
-  // Convert strResponse to object
-  useEffect(() => {
-    setResponse(JSON.parse(strResponse));
-  }, [strResponse]);
+    // Convert strResponse to object
+    useEffect(() => {
+        setResponse(JSON.parse(strResponse))
+    }, [strResponse])
 
-  /** -------------- HTTP CLIENT -------------- **/
+    /** -------------- HTTP CLIENT -------------- **/
 
-  return (
-    <div className="container">
+    return (
+        <>
+            <div className='container'>
+                <HttpClient
+                    responseCallBack={setStrResponse}
+                    errorCallBack={setError}
+                    endpoint='history'
+                />
 
-    <>
-      <HttpClient
-        responseCallBack={setStrResponse}
-        errorCallBack={setError}
-        endpoint="history"
-      />
+                <div className='App-history mt-5'>
+                    <h1>History of Space X</h1>
 
-      <div className="App-history mt-5">
-        <h1>History of Space X</h1>
+                    <div></div>
 
-        <div></div>
+                    <div className='container mt-5'>
+                        <Row>
+                            {response &&
+                                response.map((datum, id) => {
+                                    return (
+                                        <>
+                                            <CardHistory key={id} history={datum} id={id} />
+                                        </>
+                                    )
+                                })}
+                        </Row>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
 
-        <div className="container mt-5">
-          <Row>
-            {response &&
-              response.map((datum, id) => {
-                return (
-                  <>
-                    <CardHistory key={id} history={datum} id={id} />
-                  </>
-                );
-              })}
-          </Row>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default ListHistory;
+export default ListHistory
