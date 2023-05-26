73cd4048415fc12f7e5af0b7c232c0758513e7ac
Corentin Maille <maillecorentin@pm.me>
2023-05-26 16:48:19 +0200

    lint + format

--- src/components/ThemeMode.js
+++ src/components/ThemeMode.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Moon, BrightnessLow } from 'react-bootstrap-icons';
+import { Moon, BrightnessLow } from 'react-bootstrap-icons'
 
 const ThemeMode = () => {
     const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('theme') === 'dark')
@@ -8,8 +8,8 @@ const ThemeMode = () => {
         // Set / unset dark-mode
         document.body.classList.toggle('dark-mode')
         // Change button style
-        document.querySelector('#darkModeBtn').classList.toggle('btn-light');
-        document.querySelector('#darkModeBtn').classList.toggle('btn-dark');
+        document.querySelector('#darkModeBtn').classList.toggle('btn-light')
+        document.querySelector('#darkModeBtn').classList.toggle('btn-dark')
 
         localStorage.setItem('theme', isDarkMode === true ? 'dark' : 'light')
     }, [isDarkMode])
@@ -19,12 +19,13 @@ const ThemeMode = () => {
     }
 
     return (
-        <button onClick={toggleDarkMode} id={"darkModeBtn"} className='btn btn-light py-0' style={{ width: "60px", height: "40px" }}>
-            { isDarkMode ? (
-                <BrightnessLow />
-            ) : (
-                <Moon className={"mb-1"} />
-            )}
+        <button
+            onClick={toggleDarkMode}
+            id={'darkModeBtn'}
+            className='btn btn-light py-0'
+            style={{ width: '60px', height: '40px' }}
+        >
+            {isDarkMode ? <BrightnessLow /> : <Moon className={'mb-1'} />}
         </button>
     )
 }
