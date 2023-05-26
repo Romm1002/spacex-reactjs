757a1981f103945801b3dcc20c8aab8bfdd11cc2
Romm1002 <78146244+Romm1002@users.noreply.github.com>
2023-05-26 14:06:07 +0200

    quiz end (#21)

--- src/pages/QuizQuestions.js
+++ src/pages/QuizQuestions.js
@@ -1,85 +1,113 @@
-import React, { useState } from 'react'
-import quizzesData from '../data/data.json'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import quizzesData from '../data/data.json';
+import { useParams } from 'react-router-dom';
+import { shuffle } from 'lodash';
 
 const QuizQuestions = () => {
-    const { id } = useParams()
-    const quizzes = quizzesData.quizzes
-    const quiz = quizzes[id]
+  const { id } = useParams();
+  const quizzes = quizzesData.quizzes;
+  const quiz = quizzes[id];
+  const questionCount = quiz.questions.length;
 
-    const [selectedAnswers, setSelectedAnswers] = useState([])
-    const [showResults, setShowResults] = useState(false)
+  const [selectedAnswers, setSelectedAnswers] = useState(
+    Array(questionCount).fill(null)
+  );
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+  const [timer, setTimer] = useState(30);
+  const [shuffledOptions, setShuffledOptions] = useState([]);
 
-    // Fonction pour mélanger les options dans un ordre aléatoire
-    const shuffleOptions = (options) => {
-        const shuffledOptions = [...options]
-        for (let i = shuffledOptions.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1))
-            ;[shuffledOptions[i], shuffledOptions[j]] = [shuffledOptions[j], shuffledOptions[i]]
-        }
-        return shuffledOptions
+  useEffect(() => {
+    setShuffledOptions(shuffle(quiz.questions[currentQuestionIndex].options));
+  }, [currentQuestionIndex, quiz.questions]);
+
+  useEffect(() => {
+    let countdown;
+
+    if (!showResults && currentQuestionIndex < questionCount) {
+      countdown = setTimeout(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
     }
 
-    // Gérer la sélection de réponse
-    const handleAnswerSelect = (questionIndex, selectedOption) => {
-        const updatedAnswers = [...selectedAnswers]
-        updatedAnswers[questionIndex] = selectedOption
-        setSelectedAnswers(updatedAnswers)
-    }
+    return () => clearTimeout(countdown);
+  }, [timer, showResults, currentQuestionIndex, questionCount]);
 
-    // Gérer la soumission des réponses
-    const handleSubmit = () => {
-        setShowResults(true)
+  useEffect(() => {
+    if (timer === 0) {
+      handleNextQuestion();
     }
+  }, [timer]);
 
-    // Vérifier si une réponse est correcte ou incorrecte
-    const isAnswerCorrect = (questionIndex, selectedOption) => {
-        const correctAnswer = quiz.questions[questionIndex].correctAnswer
-        return selectedOption === correctAnswer
+  const handleAnswerSelect = (selectedOption) => {
+    const updatedAnswers = [...selectedAnswers];
+    updatedAnswers[currentQuestionIndex] = selectedOption;
+    setSelectedAnswers(updatedAnswers);
+    setTimer(30); // Réinitialiser le compte à rebours
+  };
+
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < questionCount - 1) {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+      setTimer(30); // Réinitialiser le compte à rebours
+    } else {
+      setShowResults(true);
     }
+  };
 
-    return (
+  const isAnswerCorrect = (questionIndex, selectedOption) => {
+    const correctAnswer = quiz.questions[questionIndex].correctAnswer;
+    return selectedOption === correctAnswer;
+  };
+
+  return (
+    <div>
+      <h2>{quiz.title}</h2>
+      {showResults ? (
         <div>
-            <h2>{quiz.title}</h2>
-            {quiz.questions.map((question, questionIndex) => (
-                <div key={questionIndex}>
-                    <h3>{question.question}</h3>
-                    {shuffleOptions(question.options).map((option, optionIndex) => (
-                        <div key={optionIndex}>
-                            <input
-                                type='radio'
-                                id={`option-${questionIndex}-${optionIndex}`}
-                                name={`question-${questionIndex}`}
-                                value={option}
-                                onChange={() => handleAnswerSelect(questionIndex, option)}
-                            />
-                            <label htmlFor={`option-${questionIndex}-${optionIndex}`}>
-                                {option}
-                            </label>
-                        </div>
-                    ))}
-                </div>
-            ))}
-            <button onClick={handleSubmit}>Submit</button>
-
-            {showResults && (
-                <div>
-                    <h3>Résultats :</h3>
-                    {quiz.questions.map((question, questionIndex) => (
-                        <div key={questionIndex}>
-                            <p>{question.question}</p>
-                            <p>Votre réponse : {selectedAnswers[questionIndex] || 'Non répondu'}</p>
-                            {isAnswerCorrect(questionIndex, selectedAnswers[questionIndex]) ? (
-                                <p>Correct!</p>
-                            ) : (
-                                <p>Incorrect. La réponse correcte est : {question.correctAnswer}</p>
-                            )}
-                        </div>
-                    ))}
-                </div>
-            )}
+          <h3>Résultats :</h3>
+          {quiz.questions.map((question, questionIndex) => (
+            <div key={questionIndex}>
+              <p>{question.question}</p>
+              <p>
+                Votre réponse :{' '}
+                {selectedAnswers[questionIndex] || 'Non répondu'}
+              </p>
+              {isAnswerCorrect(questionIndex, selectedAnswers[questionIndex]) ? (
+                <p>Correct!</p>
+              ) : (
+                <p>
+                  Incorrect. La réponse correcte est :{' '}
+                  {question.correctAnswer}
+                </p>
+              )}
+            </div>
+          ))}
         </div>
-    )
-}
+      ) : (
+        <div>
+          <h3>{quiz.questions[currentQuestionIndex].question}</h3>
+          {shuffledOptions.map((option, optionIndex) => (
+            <div key={optionIndex}>
+              <input
+                type='radio'
+                id={`option-${currentQuestionIndex}-${optionIndex}`}
+                name={`question-${currentQuestionIndex}`}
+                value={option}
+                checked={selectedAnswers[currentQuestionIndex] === option}
+                onChange={() => handleAnswerSelect(option)}
+              />
+              <label htmlFor={`option-${currentQuestionIndex}-${optionIndex}`}>
+                {option}
+              </label>
+            </div>
+          ))}
+          <p>Temps restant : {timer} secondes</p>
+          <button onClick={handleNextQuestion}>Next</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default QuizQuestions
+export default QuizQuestions;
