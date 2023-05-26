7d8678aaec67156e82b10d165bc3b2e19947bc26
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2023-05-26 09:00:08 +0200

    wip (#9)

--- src/utils/FormatDate.js
+++ src/utils/FormatDate.js
@@ -1,15 +1,24 @@
 export const FormatDate = (date) => {
-  var d = new Date(0);
+    var d = new Date(0)
 
-  d.setUTCSeconds(date);
+    d.setUTCSeconds(date)
 
-  return (
-    (d.getDate() < 10 ? "0" + d.getDate() : d.getDate()) +
-    "/" +
-    (d.getMonth() < 10 ? "0" + d.getMonth() : d.getMonth()) +
-    "/" +
-    d.getFullYear()
-  );
-};
+    return (
+        (d.getDate() < 10 ? '0' + d.getDate() : d.getDate()) +
+        '/' +
+        (d.getMonth() < 10 ? '0' + d.getMonth() : d.getMonth()) +
+        '/' +
+        d.getFullYear()
+    )
+}
 
-export default FormatDate;
+export const FormatDistance = (distance) => {
+    const formattedDistance = new Intl.NumberFormat('fr-FR', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).format(distance.toFixed(2))
+
+    return formattedDistance + ' Km'
+}
+
+export default FormatDate
